386219c5ecae06414bb0aa141a32495f4783caef
Stephen Smithstone <stephen@smithstone.co.uk>
2012-08-05 20:27:34 +0100

    working smoke test

---
+++ scoobydo_ipad/SDStatusViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDStatusViewController.h
+//  scoobydo_ipad
+//
+//  Created by Stephen Smithstone on 05/08/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDStatusViewController : UIViewController
+
+@end
---
+++ scoobydo_ipad/SDStatusViewController.m
@@ -0,0 +1,36 @@
+//
+//  SDStatusViewController.m
+//  scoobydo_ipad
+//
+//  Created by Stephen Smithstone on 05/08/2012.
+//
+//
+
+#import "SDStatusViewController.h"
+
+@interface SDStatusViewController ()
+
+@end
+
+@implementation SDStatusViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
